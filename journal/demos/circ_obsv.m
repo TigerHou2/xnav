ea243bcc3ba7dee1eb307836d0f07e353bab0ea7
TigerHou2 <44105132+TigerHou2@users.noreply.github.com>
2020-11-09 16:21:18 -0600

    circ. fitting tst

---
+++ journal/demos/circ_obsv.m
@@ -0,0 +1,53 @@
+%% circ_obsv.m
+%
+% Author:
+%   Tiger Hou
+% 
+% Description:
+%   Models the error in the estimate for the a) Radius and b) Center of a
+%   circle w.r.t. the number of observations using the hyperfit circle 
+%   fitting algorithm.
+%
+
+%% initialization
+
+close all
+clear;clc
+
+addpath('..\fcns_misc')
+
+R = 1;
+C = [0,0];
+df = deg2rad(60);
+numSims = 1500;
+noise = 5e-4;
+
+obsvVect = 3:2:40;
+
+err_R_mat = nan(numSims,length(obsvVect));
+err_C_mat = nan(numSims,length(obsvVect));
+
+for i = 1:length(obsvVect)
+    
+    numObsv = obsvVect(i);
+    
+    ncube = randn(numObsv,2,numSims);
+    ncube = ncube ./ vecnorm(ncube,2,2) .* normrnd(0,noise,1,1,numSims);
+    
+    angs = linspace(0,df,numObsv)';
+    xy = [cos(angs), sin(angs)] * R + C;
+    
+    for s = 1:numSims
+        n = ncube(:,:,s);
+        [a,b,Rest] = hyperfit_cpp(xy+n);
+        err_R_mat(s,i) = abs(R-Rest) / abs(R) * 100;
+        err_C_mat(s,i) = norm([a,b]-C) / abs(R) * 100;
+    end
+    
+end
+
+figure(1)
+plot(obsvVect, 1./sqrt(mean(err_R_mat)))
+
+figure(2)
+plot(obsvVect, 1./sqrt(mean(err_C_mat)))
---
+++ journal/demos/circ_span.m
@@ -0,0 +1,50 @@
+%% circ_span.m
+%
+% Author:
+%   Tiger Hou
+% 
+% Description:
+%   Models the error in the estimate for the a) Radius and b) Center of a
+%   circle w.r.t. the span of measurements using the hyperfit circle 
+%   fitting algorithm.
+%
+
+%% initialization
+
+close all
+clear;clc
+
+addpath('..\fcns_misc')
+
+R = 1;
+C = [0,0];
+numObsv = 17;
+numSims = 1500;
+noise = 5e-4;
+
+ncube = randn(numObsv,2,numSims);
+ncube = ncube ./ vecnorm(ncube,2,2) .* normrnd(0,noise,1,1,numSims);
+
+df_range = deg2rad(linspace(5,360,200));
+
+err_R_mat = nan(numSims,length(df_range));
+err_C_mat = nan(numSims,length(df_range));
+
+for i = 1:length(df_range)
+    angs = linspace(0,df_range(i),numObsv)';
+    xy = [cos(angs), sin(angs)] * R + C;
+    
+    for s = 1:numSims
+        n = ncube(:,:,s);
+        [a,b,Rest] = hyperfit_cpp(xy+n);
+        err_R_mat(s,i) = abs(R-Rest) / abs(R) * 100;
+        err_C_mat(s,i) = norm([a,b]-C) / abs(R) * 100;
+    end
+    
+end
+
+figure(1)
+plot(rad2deg(df_range),1./sqrt(mean(err_R_mat)))
+
+figure(2)
+plot(rad2deg(df_range),1./sqrt(mean(err_C_mat)))

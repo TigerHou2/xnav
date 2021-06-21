57cfc28224f06c587234a8bef9559302fce9ba1d
Tiger Hou <44105132+TigerHou2@users.noreply.github.com>
2021-06-21 17:35:48 -0500

    Improved objective function and Kepler's eq. solver speed, added more measurement collection options

--- range_rate/fedm_enhanced/kepler.m
+++ range_rate/fedm_enhanced/kepler.m
@@ -1,16 +1,5 @@
-function Evect = kepler(Mvect,e)
+function E = kepler(M,e)
 %KEPLER solves Kepler's equation using the Conway-Laguerre algorithm.
-
-Evect = nan(size(Mvect));
-
-for i = 1:length(Mvect(:))
-    
-M = Mvect(i);
-
-if M == 0
-    Evect(i) = 0;
-    continue
-end
 
 d = 1;
 n = 5;
@@ -18,25 +7,21 @@ n = 5;
 if e < 1
     E = mod(M+e/2,2*pi);
     while d > 1e-9
-        E = E - n*(E-e*sin(E)-M) / ...
+        E = E - n*(E-e*sin(E)-M) ./ ...
             (1-e*cos(E) + sign(1-e*cos(E)) ...
-                        * sqrt(abs((n-1)^2*(1-e*cos(E))^2 ...
-                                   -n*(n-1)*(E-e*sin(E)-M)*(e*sin(E)))));
-        d = abs(E-e*sin(E)-M);
+                       .* sqrt(abs((n-1)^2*(1-e*cos(E)).^2 ...
+                                   -n*(n-1)*(E-e*sin(E)-M).*(e*sin(E)))));
+        d = max(abs(E-e*sin(E)-M),[],'all');
     end
 else
     E = mod(M+e/2,2*pi);
     while d > 1e-9
-        E = E - n*(e*sinh(E)-E-M) / ...
+        E = E - n*(e*sinh(E)-E-M) ./ ...
             (e*cosh(E)-1 + sign(e*cosh(E)-1) ...
-                         * sqrt(abs((n-1)^2*(e*cosh(E)-1)^2 ...
-                                    -n*(n-1)*(e*sinh(E)-E-M)*(e*sinh(E)))));
-        d = abs(e*sinh(E)-E-M);
+                        .* sqrt(abs((n-1)^2*(e*cosh(E)-1).^2 ...
+                                    -n*(n-1)*(e*sinh(E)-E-M).*(e*sinh(E)))));
+        d = max(abs(e*sinh(E)-E-M),[],'all');
     end
-end
-
-Evect(i) = E;
-
 end
 
 end
--- range_rate/fedm_enhanced/test.m
+++ range_rate/fedm_enhanced/test.m
@@ -18,10 +18,10 @@ addpath('../fcns_orb')
 
 %% Define Orbit
 
-mu = 1;
-a = 1;
-e = 0.9489;
-i = deg2rad(45);
+mu = 3.986e14;
+a = (6378+600)*1e3;
+e = 0.53423;
+i = deg2rad(0);
 o = deg2rad(0);
 w = deg2rad(0);
 f0 = deg2rad(163.62);
@@ -34,24 +34,40 @@ pulsars = [ [1, 0, 0]; ... pulsar 1
             [0, 1, 1]; ... pulsar 2
             [1, 1, 4]; ... pulsar 3
             [-1, 1, -3]; ... pulsar 4
+%             [-1, -1, 2]; ... pulsar 5
           ];
-pulsarMat = pulsars' ./ vecnorm(pulsars');
-numPulsars = size(pulsars,1);
+pulsarMat = pulsars' ./ vecnorm(pulsars'); % transpose and normalize
+numPulsars = size(pulsars,1); % counter the number of pulsars
 
 %% Simulate Measurements
 
-groupMeasurements = false;
+groupMeasurements = true;  % measurements are taken in groups.
+                            % If true, each group would contain
+                            % all measurements from only one pulsar.
+                            % If false, each group would contain one
+                            % measurement from each pulsar.
+% time interval ratios
+memberInterval = 1;     % after each measurement within the same group
+groupInterval = 0;      % between groups
 
-numObsvPerPulsar = 5;
-dM = 2*pi * 0.3235;
+numObsvPerPulsar = 4;
+dM = 2*pi * 0.1235;
 E0 = 2*atan( tan(f0/2) * sqrt((1-e)/(1+e)) );
 M0 = E0 - e*sin(E0);
 Mf = M0 + dM;
-Mvect = linspace(M0,Mf,numPulsars*numObsvPerPulsar);
 if groupMeasurements
-    Mvect = reshape(Mvect,numObsvPerPulsar,numPulsars)';
+    numMembers = numObsvPerPulsar;
+    numGroups  = numPulsars;
 else
-    Mvect = reshape(Mvect,numPulsars,numObsvPerPulsar);
+    numMembers = numPulsars;
+    numGroups  = numObsvPerPulsar;
+end
+Mvect = [ 0 : (numGroups -1) ] * memberInterval * numMembers ...
+      + [ 0 : (numMembers-1) ]'* memberInterval;
+Mvect = Mvect + [ 0 : (numGroups -1) ] * groupInterval;
+Mvect = Mvect / max(Mvect(:)) * dM + M0;
+if groupMeasurements
+    Mvect = Mvect';
 end
 Evect = kepler(Mvect,e);
 Fvect = 2*atan( tan(Evect/2) * sqrt((1+e)/(1-e)) );
@@ -70,29 +86,39 @@ end
 Toffset = rand;
 Tvect = Mvect * sqrt(a^3/mu) + Toffset;
 
+%% Perturbations
+
+noise = normrnd(0,R/1000,size(rangeRateData));
+rangeRateData_truth = rangeRateData;
+rangeRateData = rangeRateData + noise;
+
 %% Test Objective Function using True Solution
 
 trueVars = [f0, e, dM];
 fVal = objFcn(trueVars, rangeRateData, Tvect, pulsarMat, mu);
-disp(['True solution error: ' num2str(fVal)])
+disp(['True solution residual: ' num2str(fVal^2)])
 
 %% Initial Guess and Search Radius
 
 initGuess = [pi, 0.5, pi];
 radius = [pi, 0.5, pi];
 fun = @(x) objFcn(x,rangeRateData,Tvect,pulsarMat,mu);
-options = optimoptions('fsolve','Display','iter' ...
+options = optimoptions('fsolve','Display','none' ...
                                ,'MaxFunctionEvaluations',12000 ...
                                ,'StepTolerance', 1e-16 ...
                                ,'FunctionTolerance', 1e-16 ...
-                               ,'OptimalityTolerance', 1e-16 ...
                                ,'MaxIterations',3000 ...
                                ,'Algorithm','levenberg-marquardt');
 iteration = 0;
+residual = fun(initGuess);
+residualPrev = 0;
 
 %% Guess and Search for Solution
+% profile on
+while 1
 
 iteration = iteration + 1;
+disp('====')
 disp(['Iteration ' num2str(iteration) ':'])
 
 lb = initGuess - radius;
@@ -108,9 +134,24 @@ range_e  = linspace(lb(2),ub(2),res(2)+1);
 range_dM = linspace(lb(3),ub(3),res(3)+1);
 
 initGuess = bounded_search(fun,range_f0,range_e,range_dM,res);
-initGuess = fsolve(fun,initGuess,options);
-if fun(initGuess) < 1e-3
-    radius = radius/2;
+if abs(residual-residualPrev) / residualPrev < 5e-2
+    initGuess = fsolve(fun,initGuess,options);
 end
-disp(['Guess:      ' num2str(initGuess)])
-disp(['True Soln.: ' num2str(trueVars)])
+residualPrev = residual;
+thisResidual = fun(initGuess)^2;
+if thisResidual <= residual
+    radius = radius/2;
+    residual = thisResidual;
+else
+    radius = radius * 0.9;
+end
+disp(['    Guess:      ' num2str(initGuess)])
+disp(['    True Soln.: ' num2str(trueVars)])
+disp(['    Residual:   ' num2str(thisResidual)])
+
+if iteration >= 30
+    break
+end
+
+end
+% profile viewer
